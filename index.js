3fecec92047b2dfc7655732dbb465494f45be624
sanjay-2611 <sanjay261101@gmail.com>
2023-03-15 21:44:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('sanjay');
+let firstName = "Sanjay";
+//alert("Hi I am " + firstName + " Welcome to our page");
+
+//Java Script is dynamic datatype based
+
+console.log(45+5);
+let name="sanjay";
+let number=12345;
+let  boolean=true;
+let some=undefined;  // you can set various variables after
+
+//Object
+page ={
+    name: "sanjay",
+    number:123456,
+    fossil: true,
+    address: "Chennai, TamilNadu, India",
+
+    childPage:{
+        name: "narmu",
+        number:7010128428,
+        fossil:false
+    }
+}
+
+
+// accessing object using dot notation
+console.log(page.number);
+
+// accessing object using square bracket notation
+console.log(page['fossil']);
+
+//accessing the child page using page object
+console.log(page.childPage.number);
