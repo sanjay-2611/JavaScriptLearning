9caef3022321c597469a5d1a121ca4b693f5effb
sanjay-2611 <sanjay261101@gmail.com>
2023-03-18 11:59:19 +0530

    UPTODATE

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-console.log('sanjay');
-let firstName = "Sanjay";
-//alert("Hi I am " + firstName + " Welcome to our page");
+// console.log('sanjay');
+// let firstName = "Sanjay";
+// //alert("Hi I am " + firstName + " Welcome to our page");
 
-//Java Script is dynamic datatype based
+// //Java Script is dynamic datatype based
 
-console.log(45+5);
-let name="sanjay";
-let number=12345;
-let  boolean=true;
-let some=undefined;  // you can set various variables after
+// console.log(45+5);
+// let name="sanjay";
+// let number=12345;
+// let  boolean=true;
+// let some=undefined;  // you can set various variables after
 
-//Object
-page ={
-    name: "sanjay",
-    number:123456,
-    fossil: true,
-    address: "Chennai, TamilNadu, India",
+// //Object
+// page ={
+//     name: "sanjay",
+//     number:123456,
+//     fossil: true,
+//     address: "Chennai, TamilNadu, India",
 
-    childPage:{
-        name: "narmu",
-        number:7010128428,
-        fossil:false
-    }
+//     childPage:{
+//         name: "narmu",
+//         number:7010128428,
+//         fossil:false
+//     }
+// }
+
+
+// // accessing object using dot notation
+// console.log(page.number);
+
+// // accessing object using square bracket notation
+// console.log(page['fossil']);
+
+// //accessing the child page using page object
+// console.log(page.childPage.number);
+// console.log("github");
+// console.log(1234);
+let name="sanjay"
+let age=22
+function method (name, age)
+{
+    console.log(name+age)
 }
 
+method("sanjay ", 22)
 
-// accessing object using dot notation
-console.log(page.number);
+if(age>18 && name=="sanjay")
 
-// accessing object using square bracket notation
-console.log(page['fossil']);
-
-//accessing the child page using page object
-console.log(page.childPage.number);
-console.log("github");
-console.log(1234);
+    console.log(true);
+else
+    console.log(false);
